44069118d90e9a57a343037c5a4346f1c979f45b
Sandeep Singh Bhandari <sandybhndr05@gmail.com>
2025-06-16 20:53:00 +0800

    Added Spring Boot Project

---
+++ src/main/java/com/springboot/gic/MineSweeperApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.gic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineSweeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MineSweeperApplication.class, args);
+	}
+
+}
